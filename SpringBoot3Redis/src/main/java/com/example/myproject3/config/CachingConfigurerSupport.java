b604742082c798a1054a9007920ddfaf0e352d9d
DINGJUN <dingjun@chinatvpay.com>
2019-01-21 11:47:52 +0800

    SpringBoot Redis 使用测试2

--- SpringBoot3Redis/src/main/java/com/example/myproject3/config/CachingConfigurerSupport.java
+++ SpringBoot3Redis/src/main/java/com/example/myproject3/config/CachingConfigurerSupport.java
@@ -1,11 +1,18 @@
 package com.example.myproject3.config;
 
-import org.springframework.boot.autoconfigure.session.RedisSessionProperties;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 
 import java.lang.reflect.Method;
 
@@ -14,7 +21,7 @@ import java.lang.reflect.Method;
 @EnableCaching
 public class CachingConfigurerSupport {
     @Bean
-    public KeyGenerator keyGenerator(){
+    public KeyGenerator keyGenerator() {
         return new KeyGenerator() {
             @Override
             public Object generate(Object target, Method method, Object... params) {
@@ -30,11 +37,22 @@ public class CachingConfigurerSupport {
         };
     }
 
-//    @Bean
-//    public CacheManager cacheManager(){
-//        new ca
-//        return cacheManager;
-//    }
+    @Bean
+    public CacheManager cacheManager(RedisTemplate redisTemplate) {
+        // TODO 这个构造函数没有需要一个RedisTemplate的？还是依赖错了。
+        CacheManager cacheManager = new RedisCacheManager(null, null);
+        return cacheManager;
+    }
 
-//    public rediste
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
+        StringRedisTemplate template = new StringRedisTemplate(factory);
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        template.afterPropertiesSet();
+        return template;
+    }
 }
