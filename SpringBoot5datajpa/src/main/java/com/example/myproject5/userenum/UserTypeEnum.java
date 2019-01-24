11e420bed856a80589cb1b7589587df9e3844ed5
DINGJUN <dingjun@chinatvpay.com>
2019-01-24 13:22:31 +0800

    SpringBoot5 枚举的使用练习

---
+++ SpringBoot5datajpa/src/main/java/com/example/myproject5/userenum/UserTypeEnum.java
@@ -0,0 +1,14 @@
+package com.example.myproject5.userenum;
+
+public enum UserTypeEnum {
+    student,worker,teacher;
+
+    UserTypeEnum() {
+    }
+
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
