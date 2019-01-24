ea4c7af1aba4f18b5c47f55c465a4f5d97b9011e
DINGJUN <dingjun@chinatvpay.com>
2019-01-24 13:21:37 +0800

    SpringBoot5 datajpa 测试练习完成

--- SpringBoot5datajpa/src/main/java/com/example/myproject5/dao/UserRepository.java
+++ SpringBoot5datajpa/src/main/java/com/example/myproject5/dao/UserRepository.java
@@ -2,6 +2,8 @@ package com.example.myproject5.dao;
 
 import com.example.myproject5.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -26,5 +28,29 @@ public interface UserRepository extends JpaRepository<User, Long> {
      * 3.自定义sql查询
      * 4.多表查询
      */
+
+    /**
+     * 限制查询
+     */
+    User findTopByUserName(String userName);
+
+    /**
+     * 自定义sql，自定义sql中需要注意的是原生sql还是hql
+     */
+//    @Modifying 这个注解用来标识是create,update,modify的,查询不能使用
+    // hql
+    @Query(value = "select u  from User u where u.userName = ?1",nativeQuery = false)
+    User findByUserName(String userName);
+    // sql
+    @Query(value = "select * from user where user_name = ?1",nativeQuery = true)
+    User findByUserName2(String userName);
+
+    /**
+     * 多表查询
+     */
+    //定义结果集接口--接口中提供查询结果的字段的get方法--自定义sql--返回结果集使用Page<自定义结果集接口>
+    //spring会生成一个动态代理来接受结果集。
+
+
 }
 
--- SpringBoot5datajpa/src/test/java/com/example/UserRepositoryTest.java
+++ SpringBoot5datajpa/src/test/java/com/example/UserRepositoryTest.java
@@ -7,8 +7,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -16,22 +21,61 @@ import java.util.List;
 public class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
+
     /**
      * spring data jpa 中提供了CRUD基本的数据库的操作方法。默认的方法。
      */
     @Test
-    public void crudTest(){
+    public void crudTest() {
         List<User> list = userRepository.findAll();
         System.out.println(list);
     }
+
     /**
      * 自定义方法
      */
     @Test
-    public void cusTest(){
+    public void cusTest() {
         User us = userRepository.findByNickName("cc123456");
         System.out.println(us);
         User user = userRepository.findByUserNameAndNickName("cc3", "cc123456");
         System.out.println(user);
     }
+
+    /**
+     * 复杂查询
+     */
+    @Test
+    public void complicatedTest() {
+        //分页查询
+        Sort sort = new Sort(Sort.Direction.DESC, "id");
+        PageRequest pageRequest = new PageRequest(1, 1, sort);
+        Page<User> pageable = userRepository.findAll(pageRequest);
+        ArrayList<User> list = new ArrayList<>();
+        for (User user :
+                pageable) {
+            list.add(user);
+        }
+        System.out.println("-----------------分页查询---------------");
+        System.out.println(list);
+        System.out.println("-----------------分页查询---------------");
+
+        // 限制查询。查询前N条数据，或第一条数据。
+        //使用命名规则方式查询
+//        User findFirstByOrderByLastnameAsc();
+//        User findTopByOrderByAgeDesc();
+//        Page<User> queryFirst10ByLastname(String lastname, Pageable pageable);
+//        List<User> findFirst10ByLastname(String lastname, Sort sort);
+//        List<User> findTop10ByLastname(String lastname, Pageable pageable);
+
+        // 自定义sql
+        User cc3 = userRepository.findByUserName("cc3");
+        User cc4 = userRepository.findByUserName2("cc3");
+        System.out.println("-----------------自定义sql---------------");
+        System.out.println(cc3);
+        System.out.println(cc4);
+        System.out.println("-----------------自定义sql---------------");
+
+    }
+
 }
